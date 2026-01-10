dc9bb2efbd6c7b06f783d29f4ed7fa15cac27ab7
Reto Stauffer <Reto.Stauffer@uibk.ac.at>
2026-01-10 09:35:58 +0100

    dev: Working on loading and analyzing single bike position tracks to filter out bad data

--- fixdb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sqlite3 stadtrad.db .dump > stadtrad_fixed.db
--- stadtrad/R/db.R
+++ stadtrad/R/db.R
@@ -53,10 +53,31 @@ sr_connect <- function(file) {
 #'        Used to (i) transform `start` and `end` to POSIXct if needed,
 #'        as well as to properly convert the data returned.
 #'
+#' @examples
+#' \dontrun{
+#' ## Connecting to database
+#' con <- sr_connect("stadtrad.db")
+#'
+#' ## Define date range
+#' start <- as.POSIXct("2025-12-22 23:00", tz = "Europe/Vienna")
+#' end   <- as.POSIXct("2025-12-24 01:00", tz = "Europe/Vienna")
+#'
+#' ## Loading all bike information in time period
+#' d <- sr_load_data(con, start = start, end = end, tz = "CET") 
+#'
+#' ## Loading data for one specific bike by ID (number)
+#' d <- sr_load_data(con, numbers = 340000, start = start, end = end, tz = "CET")
+#' }
+#'
+#' @return If no data are found, `NULL` is returned. Else an object of class
+#' `c("srbike", "sf", "data.frame")`, i.e., a simple features data frame
+#' with an additional stadtrad bike position class to provide S3 support.
+#'
 #' @author Reto
 #' @export
 #'
 #' @importFrom RSQLite dbGetQuery
+#' @importFrom sf st_as_sf st_crs
 sr_load_data <- function(con, numbers = NULL, start = NULL, end = NULL, tz = "") {
 
     stopifnot(
@@ -73,6 +94,10 @@ sr_load_data <- function(con, numbers = NULL, start = NULL, end = NULL, tz = "")
         end <- tryCatch(as.POSIXct(end[1L], tz = tz),
                           error = function(e) stop("Error converting 'end'": e))
 
+    if (!is.null(start) && !is.null(end))
+        stopifnot("'start' must be before 'end'" = start < end)
+
+    # Auxilary function to create the required SQLite query
     get_query <- function(start, end) {
         sql <- "SELECT b.first_seen, b.last_seen, b.number,
                 b.bike_type, b.active, p.name, p.lon, p.lat
@@ -81,27 +106,28 @@ sr_load_data <- function(con, numbers = NULL, start = NULL, end = NULL, tz = "")
         # sql <- gsub("\\s{2,}", " ", sql)
         where <- list()
         if (!is.null(start))
-            where <- c(where, sprintf("min(b.first_seen, b.last_seen) < %d", as.integer(start)))
+            where <- c(where, sprintf("min(b.first_seen, b.last_seen) > %d", as.integer(start)))
         if (!is.null(end))
-            where <- c(where, sprintf("max(b.first_seen, b.last_seen) >= %d", as.integer(end)))
+            where <- c(where, sprintf("max(b.first_seen, b.last_seen) <= %d", as.integer(end)))
 
         # Number search: ensure 'number' is handled as integer, not index
         if (!is.null(numbers))
             where <- c(where, paste0("b.number + 0 IN (", paste(numbers, collapse = ", "), ")"))
 
         # Gluing together the where clause
-        if (length(where > 0))
+        if (length(where) > 0)
             sql <- paste(sql, "WHERE", paste(where, collapse = "\n   AND "))
 
         return(paste(sql, "\n     ORDER BY b.number, b.first_seen"))
     }
 
     sql <- get_query(start, end)
-    cat("------------------\n", sql, "\n----------------\n")
+    ##cat("------------------\n", sql, "\n----------------\n")
 
     # Requesting data
     data <- tryCatch(dbGetQuery(con, sql),
                      error = function(e) stop("Error querying data:", e))
+    if (nrow(data) == 0) return(NULL)
 
     # Transforming first/last seen
     data$first_seen <- as.POSIXct(data$first_seen, tz = tz)
@@ -110,6 +136,59 @@ sr_load_data <- function(con, numbers = NULL, start = NULL, end = NULL, tz = "")
     # Coerce to simple feature data.frame
     data <- st_as_sf(data, coords = c("lon", "lat"), crs = st_crs(4326))
 
-    return(data)
+    return(structure(data, class = c("srbike", class(data))))
 }
+
+
+#' @rdname sr_load_data
+#'
+#' @exportS3Method plot srbike
+#'
+#' @importFrom sf st_coordinates
+#' @importFrom stats setNames
+plot.srbike <- function(x, ...) {
+
+    fn <- function(x) {
+        # Ensuring order
+        x <- x[order(x$first_seen), ]
+        geom   <- st_geometry(x)
+        coord  <- st_coordinates(geom)
+        distkm <- 1/1000 * c(NA_real_, st_distance(geom[-1], geom[-length(geom)], by_element = TRUE))
+        timeh  <- c(NA_integer_, as.numeric(x$first_seen[-1L] - x$last_seen[-nrow(x)], unit = "hours"))
+        data.frame(number     = x$number,
+                   fist_seen  = x$first_seen,
+                   last_seen  = x$last_seen,
+                   lon        = coord[, 1L],
+                   lat        = coord[, 2L],
+                   dist_km    = round(distkm, 3),
+                   parked_min = as.numeric(x$last_seen - x$first_seen, unit = "mins"),
+                   timediff_h = round(timeh, 3),
+                   speed_kmh  = round(distkm / timeh, 3))
+    }
+
+    x <- setNames(lapply(split(x, x$number), fn), unique(x$number))
+    return(x)
+
+}
+
+#' Converting Stadtrad Bike Positions to Dataframe (Long Format)
+#'
+#' @param x Object of class `srbike` as returned by [sr_load_data()].
+#'
+#' @author Reto
+#' @export
+#'
+#' @importFrom sf st_coordinates
+sr_as_df <- function(x, ...) {
+    stopifnot("argument 'x' must be an object of class 'srbike'" = 
+              inherits(x, "srbike"))
+
+    df <- data.frame(number = rep(x$number, 4L),
+                     coord  = rep(as.vector(st_coordinates(x)), each = 2L),
+                     ctype  = rep(c("longitude", "latitude"), each = nrow(x) * 2L),
+                     timestamp = rep(c(x$first_seen, x$last_seen), each = 2L))
+
+    return(df[order(df$timestamp), ])
+}
+
 
