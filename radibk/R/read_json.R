5edd9a95856d2e5b249e9aaff85038a339f55d9c
Reto Stauffer <reto.stauffer@uibk.ac.at>
2026-07-29 21:02:32 +0200

    dev: Working on ri_read_*

--- radibk/R/read_json.R
+++ radibk/R/read_json.R
@@ -1,27 +1,47 @@
 
 
-#' @param x character, name/path of one single json file (or multiple files when calling
-#'        [ri_read_jsons()]) with the expected content.
-#'        The file names, as its first integer, must contain a timestamp (seconds UTC) which
-#'        is extracted as temporal information.
+#' @param x character, name/path of the file(s) to be read. See Section
+#'        Details for more information.
 #' @param city_name character of length one, defaults to `"Innsbruck"`. Used to
 #'        subset the data to one specific city.
-#' @param cores `NULL` (default) or integer, only used by [ri_read_jsons()].
-#'        Number of cores to use with `parallel::mclapply`. If `NULL` takes all but
-#'        two available cores (maximum number of files to be processed).
-#' @param returnclass one of "`data.frame"` (tibble data frames, default)
-#'        or `"list"` (only for [ri_read_json()]).
-#' @param verbose Logical, defaults to `FALSE`.
+#' @param cores `NULL` (default) or integer, not used by `ri_read_json`.
+#'        Number of cores to used with `parallel::mclapply`. If `NULL` it takes all but
+#'        two available cores (topped of by the maximum number of json files to be
+#'        processed).
+#' @param returnclass only used by `ri_read_json`, one of "`data.frame"`
+#'        (tibble data frames, default) or `"list"` (only for `ri_read_json`).
+#' @param verbose integer, defaults to `0L` (`FALSE`; silent). If it evaluates to
+#'        `1L` (`TRUE`) it is verbose on function level, `2L` makes the interfaced
+#'        functions verbose, too.
 #'
 #' @return A named list is returned which by default contains three
 #' tibble data frames, namely `cities`, `places`, and `bikes`. The
 #' date and time information extracted from the file name(s) `x` is
 #' added as a new variable `datetime`.
 #'
-#' Calling [ri_read_json()] with `returnclass = "list"` the return is also
+#' Calling `ri_read_json` with `returnclass = "list"` the return is also
 #' a named list but with four elements containing the raw data from the json
 #' file subsetted to the city requested, plus one additional element
 #' `datetime` with the timestamp (POSIXt).
+#'
+#' @examples
+#' \dontrun{
+#' ## Find available json files
+#' jsonfiles <- list.jsonfiles(file.path("archive", "foo"), full.names = TRUE)
+#' jsonfiles <- jsonfiles[grepl("^[0-9]+_si.json$", basename(jsonfiles))]
+#'
+#' ## Reading single json file
+#' x <- ri_read_json(jsonfiles[1], verbose = TRUE)
+#'
+#' ## Reading set of json files
+#' y <- ri_read_jsons(jsonfiles[1:3], verbose = TRUE)
+#'
+#' ## Quick check
+#' library("tinyplot")
+#' tinyplot(available_bikes ~ datetime, data = x$cities)
+#' tinyplot(bikes ~ datetime | name, type = "l",
+#'          data = subset(x$places, !grepl("^BIKE", name)))
+#' }
 #'
 #' @importFrom tibble as_tibble
 #' @importFrom jsonlite read_json
@@ -31,25 +51,31 @@
 #' @export
 ri_read_jsons <- function(x, city_name = "Innsbruck", cores = NULL, verbose = FALSE) {
 
+    verbose <- as.integer(verbose)[[1L]]
     if (!is.null(cores)) cores <- as.integer(cores)[[1L]]
     stopifnot(
         "argument `cores` must be NULL or integer" =
-            is.null(cores) || (is.integer(cores) && length(cores) == 1L)
+            is.null(cores) || (is.integer(cores) && length(cores) == 1L),
+        "argument `verbose` must evaluate to 0L, 1L, or 2L" = 
+            is.integer(verbose) && (verbose >= 0L && verbose <= 2L)
     )
+
+    # Setting number of cores
     if (!is.null(cores) && cores < 1L) stop("`cores` must be positive")
     if (is.null(cores)) cores <- pmin(length(x), pmax(1L, detectCores()))
 
-    if (verbose)
-        message("Processing ", length(x), " files on ", cores, " cores")
+    if (verbose) message("Processing ", length(x), " files on ", cores, " cores")
 
     # Parsing files
-    x <- lapply(x, ri_read_json, city_name = city_name, verbose = verbose)
+    x <- lapply(x, ri_read_json, city_name = city_name, verbose = verbose >= 2L)
 
     # Combining data
     x <- list(cities  = bind_rows(lapply(x, function(x) x$cities)),
               places  = bind_rows(lapply(x, function(x) x$places)),
               bikes   = bind_rows(lapply(x, function(x) x$bikes)))
-    return(x)
+
+    # Ensure correct order of rows (only based on datetime) and return
+    return(lapply(x, function(x) x[order(x$datetime, decreasing = TRUE), ]))
 }
 
 
